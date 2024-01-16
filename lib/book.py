e309093d7b628c73ff7074d8ea2c455f7ef1a2ed
mmwandera <mmwandera74@gmail.com>
2024-01-16 20:04:10 +0300

    completed assignment

--- lib/book.py
+++ lib/book.py
@@ -6,10 +6,13 @@ class Book:
         self.title = title
         self.page_count = page_count
 
+     # @property decorator is a built-in decorator in Python which is helpful in defining the properties effortlessly without manually calling the inbuilt function property(). Refer to this YT video for further explanation https://youtu.be/Sz9G0iwwHQI?si=gEaLS5iCkCLiJ9UJ
     @property
     def page_count(self):
+        # In Python, by convention, when you add a leading underscore to a name (self._page_count in this case), you are telling other developers that it should not be accessed or modified directly outside of the class. It should only be accessed through intermediaries (getters and setters) if they are available.
         return self._page_count
     
+    # Setter Method
     @page_count.setter
     def page_count(self, page_count):
         if type(page_count) == int:
--- lib/shoe.py
+++ lib/shoe.py
@@ -5,11 +5,15 @@ class Shoe:
     def __init__(self, brand, size):
         self.size = size
         self.brand = brand
-
+    # @property decorator is a built-in decorator in Python which is helpful in defining the properties effortlessly without manually calling the inbuilt function property(). Refer to this YT video for further explanation https://youtu.be/Sz9G0iwwHQI?si=gEaLS5iCkCLiJ9UJ
+      
+    # Getter Method
     @property
     def size(self):
+        # In Python, by convention, when you add a leading underscore to a name (self._size in this case), you are telling other developers that it should not be accessed or modified directly outside of the class. It should only be accessed through intermediaries (getters and setters) if they are available.
         return self._size
     
+    # Setter Method
     @size.setter
     def size(self, size):
         if type(size) == int:
